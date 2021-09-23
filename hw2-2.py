6f2e999581c47270e0d2615effa275834a0c065b
p22srogers <90051572+p22srogers@users.noreply.github.com>
2021-09-23 10:21:50 -0400

    Add files via upload

---
+++ hw2-2.py
@@ -0,0 +1,9 @@
+# Author: SMR (AMDG) 09/21/21
+free_throw= 3
+regular_shot=5
+three_pointer=5
+score= (free_throw)+((regular_shot)*2)+((three_pointer)*3)
+print("The player scored",score,"points in the game.")
+
+
+
